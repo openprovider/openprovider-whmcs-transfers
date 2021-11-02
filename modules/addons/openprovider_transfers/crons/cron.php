3356c5fc24c0742975266ce74e7096a2b31d501d
Danila <sibgat.d.s@gmail.com>
2021-11-02 11:09:00 +0700

    updated cron script

--- modules/addons/openprovider_transfers/crons/cron.php
+++ modules/addons/openprovider_transfers/crons/cron.php
@@ -3,27 +3,23 @@
 if (!function_exists('is_cli')) {
     function is_cli()
     {
-        if ( defined('STDIN') )
-        {
+        if (defined('STDIN')) {
             return true;
         }
 
-        if ( php_sapi_name() === 'cli' )
-        {
+        if (php_sapi_name() === 'cli') {
             return true;
         }
 
-        if ( array_key_exists('SHELL', $_ENV) ) {
+        if (array_key_exists('SHELL', $_ENV)) {
             return true;
         }
 
-        if ( empty($_SERVER['REMOTE_ADDR']) and !isset($_SERVER['HTTP_USER_AGENT']) and count($_SERVER['argv']) > 0)
-        {
+        if (empty($_SERVER['REMOTE_ADDR']) and !isset($_SERVER['HTTP_USER_AGENT']) and count($_SERVER['argv']) > 0) {
             return true;
         }
 
-        if ( !array_key_exists('REQUEST_METHOD', $_SERVER) )
-        {
+        if (!array_key_exists('REQUEST_METHOD', $_SERVER)) {
             return true;
         }
 
@@ -57,11 +53,11 @@ $scheduledDomains = Capsule::select("
            motsdt.informed_below_two_weeks,
            tbldomains.expirydate
     from mod_openprovider_transfers_scheduled_domain_transfer as motsdt
-    left join tbldomains
+    inner join tbldomains
     on motsdt.domain_id = tbldomains.id
     where motsdt.domain_id and motsdt.op_status <> 'ACT' and motsdt.op_status <> 'FAI'
 ");
-//
+
 //$scheduledDomains = [
 //    new stdClass(),
 //    new stdClass(),
@@ -104,42 +100,42 @@ foreach ($scheduledDomains as $scheduledDomain) {
     ]);
 
     // Update status in mod_openprovider_transfers_scheduled_domain_transfer table
-    Capsule::update("
-        update mod_openprovider_transfers_scheduled_domain_transfer
-        set op_status='{$domainOp['status']}',
-            synced_at='{$syncedAt->toDateTimeString()}'
-        where domain_id={$scheduledDomain->domain_id}
-    ");
+    Capsule::table('mod_openprovider_transfers_scheduled_domain_transfer')
+        ->where('domain_id', $scheduledDomain->domain_id)
+        ->update([
+            'op_status' => $domainOp['status'],
+            'synced_at' => $syncedAt->toDateTimeString(),
+        ]);
 
     switch ($domainOp['status']) {
         case 'ACT':
-            Capsule::update("
-                update mod_openprovider_transfers_scheduled_domain_transfer
-                set finished_transfer_date='{$syncedAt->toDateString()}'
-                where domain_id={$scheduledDomain->domain_id}
-            ");
+            // Set finished transfer date today
+            // And set domain status active
+            Capsule::table('mod_openprovider_transfers_scheduled_domain_transfer')
+                ->where('domain_id', $scheduledDomain->domain_id)
+                ->update([
+                    'finished_transfer_date' => $syncedAt->toDateString(),
+                ]);
 
-            Capsule::update("
-                update tbldomains
-                set status='Active'
-                where id={$scheduledDomain->domain_id}
-            ");
-
+            Capsule::table('tbldomains')
+                ->where('id', $scheduledDomain->domain_id)
+                ->update([
+                    'status' => 'Active',
+                ]);
             break;
         case 'PEN':
-            Capsule::update("
-                update tbldomains
-                set status='Pending Transfer'
-                where id={$scheduledDomain->domain_id}
-            ");
-
+            Capsule::table('tbldomains')
+                ->where('id', $scheduledDomain->domain_id)
+                ->update([
+                    'status' => 'Pending Transfer',
+                ]);
             if ($scheduledDomain->informed_below_two_weeks) {
                 break;
             }
 
             // If expiry date is less than two weeks
             // we need to create todoitem to check if domain ok
-            if ($syncedAt->subDays(14)->toDateString() < Carbon::createFromFormat('Y-m-d', $scheduledDomain->expirydate)->toDateString()) {
+            if ($syncedAt->toDateString() > Carbon::createFromFormat('Y-m-d', $scheduledDomain->expirydate)->subDays(14)->toDateString()) {
                 Capsule::table('tbltodolist')
                     ->insert([
                         'title' => 'Check transfer completed',
@@ -149,20 +145,20 @@ foreach ($scheduledDomains as $scheduledDomain) {
                         'duedate' => $syncedAt->toDateString(),
                     ]);
 
-                Capsule::update("
-                    update mod_openprovider_transfers_scheduled_domain_transfer
-                    set informed_below_two_weeks=1
-                    where domain_id={$scheduledDomain->domain}
-                ");
+                Capsule::table('mod_openprovider_transfers_scheduled_domain_transfer')
+                    ->where('domain_id', $scheduledDomain->domain_id)
+                    ->update([
+                        'informed_below_two_weeks' => 1
+                    ]);
             }
             break;
         case 'FAI':
-            Capsule::update("
-                update tbldomains
-                set status='Active',
-                    registrar='{$scheduledDomain->prev_registrar}'
-                where id={$scheduledDomain->domain_id}
-            ");
+            Capsule::table('tbldomains')
+                ->where('id', $scheduledDomain->domain_id)
+                ->update([
+                    'status' => 'Active',
+                    'registrar' => $scheduledDomain->prev_registrar
+                ]);
             break;
     }
 }
